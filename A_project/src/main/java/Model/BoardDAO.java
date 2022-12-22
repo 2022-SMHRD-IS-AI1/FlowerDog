beff1ca971f902a9b2eaf7c40c0a2e142d523867
smhrd <smhrd@222.102.43.209>
2022-12-22 11:02:40 +0900

    g

--- A_project/src/main/java/Model/BoardDAO.java
+++ A_project/src/main/java/Model/BoardDAO.java
@@ -9,44 +9,46 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
 
 public class BoardDAO {
+	public void getConn() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String DB_User = "cgi_4_1220_1";
+			String DB_Password = "smhrd1";
+			
+			conn = DriverManager.getConnection(url, DB_User, DB_Password);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+//==============================================================================================================
+	// 변수
 	Connection conn = null;
 	PreparedStatement psmt = null;
 	ResultSet rs = null;
 	int cnt = 0;
 	ArrayList<BoardDTO> list = new ArrayList<>();
 	
-	public void getConn() {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
-			String DB_User = "cgi_4_1220_1";
-			String DB_Password = "smhrd1";
-			
-//			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-//			String DB_User = "hr";
-//			String DB_Password = "hr";
-			
-			conn = DriverManager.getConnection(url, DB_User, DB_Password);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	// 객체를 종료하기 위한 close()
+	// SQL문
+	String inser_sql = "insert into TB_COMMUNITY values(TB_COMMENT_SEQ.nextval, ?, ?, sysdate, ?)";
+	
+	String Array_sql = "select * from TB_COMMUNITY";
+	
+	String Update_SQL = "UPDATE TB_COMMUNITY SET COMM_TITLE=?, COMM_CONTENT=?, sysdate WHERE MB_ID=?";
+	
+	
+//==============================================================================================================
 	public void close() {
 		try {
-			// ResultSet 닫기
 			if (rs != null) {
 				rs.close();
 			}
-			// PreparedStatement 닫기
 			if (psmt != null) {
 				psmt.close();
 			}
-			// Connection 닫기
 			if (conn != null) {
 				conn.close();
 			}
@@ -54,21 +56,16 @@ public class BoardDAO {
 			e2.printStackTrace();
 		}
 
-	
-	}public int insertBoard(BoardDTO b_dto) {
-		try {
+	}
 
+	public int insertBoard(BoardDTO b_dto) {
+		try {
 			getConn();
-			
-			// 3) SQL문 실행 준비
-			String sql = "insert into TB_COMMUNITY values(TB_COMMENT_SEQ.nextval, ?, ?, sysdate, ?)";
-			
-		
-			psmt = conn.prepareStatement(sql);
+			psmt = conn.prepareStatement(inser_sql);
 			psmt.setString(1, b_dto.getTitle());
 			psmt.setString(2, b_dto.getContent());
 			psmt.setString(3, b_dto.getWriter());
-
+			
 			cnt = psmt.executeUpdate();
 
 		} catch (Exception e) {
@@ -77,62 +74,45 @@ public class BoardDAO {
 			close();
 		}
 		return cnt;
-		
-	}public ArrayList<BoardDTO> Listofposts () {
+
+	}
+
+	public ArrayList<BoardDTO> Listofposts() {
 		try {
-
 			getConn();
-
-			// 3) SQL문 실행 준비
-			String sql = "select * from TB_COMMUNITY";
-			
-			psmt = conn.prepareStatement(sql);
-			
-
+			psmt = conn.prepareStatement(Array_sql);
 			rs = psmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int number = rs.getInt(1);
 				String title = rs.getString(2);
 				String writer = rs.getString(3);
 				String content = rs.getString(4);
 				String time = rs.getString(5);
-				
 				BoardDTO dto = new BoardDTO(number, title, writer, content, time);
-				
+
 				list.add(dto);
 			}
-			
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
 		return list;
+	}
+
+	public int update(BoardDTO b_dto) {
 		
-	}public void Modify(BoardDTO dto) {
 		try {
-
-			getConn();
-
-			// 3) SQL문 실행 준비
-			String sql = "select * from web_message where id=?";
-			
-			psmt = conn.prepareStatement(sql);
-			
-			psmt.setInt(1, dto.getNumber());
-			psmt.setString(2, dto.getTitle());
-			psmt.setString(3, dto.getWriter());
-			psmt.setString(4, dto.getContent());
-			psmt.setString(5, dto.getTime());
-
-			cnt = psmt.executeUpdate();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			close();
+		    psmt = conn.prepareStatement(Update_SQL);  
+		    psmt.setString(1, b_dto.getTitle()); //게시글 제목
+		    psmt.setString(2, b_dto.getContent()); //게시글 내용
+		    psmt.setString(3, b_dto.getWriter()); //게시글 작성자
+		    
+		    return psmt.executeUpdate();
+		}catch(Exception e) {
+		    e.printStackTrace();
 		}
-}
+		return -1; //데이터베이스 오류발생
+	}
 }
--- A_project/src/main/webapp/BoardMain.jsp
+++ A_project/src/main/webapp/BoardMain.jsp
@@ -17,12 +17,28 @@
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		
 </head>
-<body>		
+<body>	
+<script language='javascript'>
+
+function noEvent() {
+    if (event.keyCode == 116) {
+        event.keyCode= 2;
+        return false;
+    }
+    else if(event.ctrlKey && (event.keyCode==78 || event.keyCode == 82))
+    {
+        return false;
+    }
+}
+document.onkeydown = noEvent;
+
+</script>	
 			<!-- Q17. 게시글 목록 조회 기능 -->
 			<!-- Q18. 게시글 목록 세부페이지 기능(제목을 클릭하면 세부페이지 BoardDetail.jsp로 이동)-->
 			<div id="board">
 <%
 	MemberDTO info = (MemberDTO)session.getAttribute("info");
+	BoardDTO dto = new BoardDTO();
 %> 
 
 					<%
@@ -31,8 +47,6 @@
 					ArrayList<BoardDTO> list = new ArrayList<>();
 					%>
 					<%if(info != null){
-						// 로그인 된 회원이 받은 메세지 띄워주기
-						// DB로 바로 접근하기!
 						list = dao.Listofposts();
 					}%>
 				
@@ -58,9 +72,11 @@
 				<a href="index.html"><button id="writer">홈으로가기</button></a>
 				<a href="BoardWrite.jsp"><button id="writer">작성하러가기</button></a>
 				
-				<% if (info != null) {%>
-				<a href="Board_Removal"><button id="writer">게시글 수정하기</button></a>
-				<a href="Board_Modify"><button id="writer">게시글 삭제하기</button></a>
+				<% if (info != null) {
+					if (info.getId().equals(dto.getWriter())){%>
+						<a href="Board_Removal"><button id="writer">게시글 수정하기</button></a>
+						<a href="Board_Modify"><button id="writer">게시글 삭제하기</button></a>
+					<%}%>
 				<%}else{%>
 				<li>로그인을 하세요.</li>
 				<%}%>
