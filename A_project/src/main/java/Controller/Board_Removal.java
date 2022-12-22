876e4f835e7775f4bbbd9d6d21f13239ad3ee4ca
USER11 <USER11@211.105.165.123>
2022-12-22 12:21:05 +0900

    게시글삭제기능푸시

--- A_project/src/main/java/Controller/Board_Removal.java
+++ A_project/src/main/java/Controller/Board_Removal.java
@@ -34,7 +34,7 @@ public class Board_Removal extends HttpServlet {
 		if(result >0) {
 			System.out.println("게시글삭제 성공");
 		}else {
-			System.out.println("게시글삭제 실패");
+			System.out.println("게시글삭제실패");
 		}
 		
 		
